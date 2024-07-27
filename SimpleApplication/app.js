842dd7233b49baae49ecc9586b13e77f6f455dea
sumafujiko <sumafujiko@gmail.com>
2024-07-27 15:15:47 +0100

    App.js edits

--- SimpleApplication/app.js
+++ SimpleApplication/app.js
@@ -3,10 +3,19 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var https = require('https'); 
+var http = require('http'); 
+var fs = require('fs'); 
 
 var indexRouter = require('./routes/index');
 
 var app = express();
+
+// HTTPS Options
+const options = {
+  key: fs.readFileSync('/path/to/your/ssl/key.pem'),
+  cert: fs.readFileSync('/path/to/your/ssl/cert.pem') 
+};
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -26,9 +35,9 @@ app.use(function(req, res, next) {
 });
 
 app.use((req, res, next) => {
-  res.set('Cache-Control', 'no-store')
-  next()
-})
+  res.set('Cache-Control', 'no-store');
+  next();
+});
 
 // error handler
 app.use(function(err, req, res, next) {
@@ -39,6 +48,19 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
+});
+
+// Create an HTTP server that redirects to HTTPS
+http.createServer((req, res) => {
+  res.writeHead(301, { Location: `https://${req.headers.host}${req.url}` });
+  res.end();
+}).listen(80, () => {
+  console.log('HTTP server listening on port 80 and redirecting to HTTPS');
+});
+
+// Create an HTTPS server
+https.createServer(options, app).listen(8443, () => {
+  console.log('HTTPS server listening on port 8443');
 });
 
 module.exports = app;
